1a4ef86b36f585e24bbf03dc3373ee23e01054c9
waltertheves <wbtheves@hotmail.com>
2020-06-25 01:12:09 -0300

    adicionei a quarta linha. TEste

--- aquele_teste_maroto.R
+++ aquele_teste_maroto.R
@@ -3,3 +3,5 @@ example <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8), nrow = 4, ncol = 2)
 example <- matrix(c(1, 1, 2, 3, 5, 8, 13, 21), nrow = 4, ncol = 2)
 
 example <- matrix(c(1, 1, 2, 3, 5, 8, 13, 10000), nrow = 4, ncol = 2)
+
+example <- matrix(c(111, 1, 222, 3, 5000, 8, 13, 12345), nrow = 4, ncol = 2)
