7218823469355799f2792c3bfdfd871d784b843c
waltertheves <wbtheves@hotmail.com>
2020-06-25 01:05:46 -0300

    terceira linha adicionada

--- aquele_teste_maroto.R
+++ aquele_teste_maroto.R
@@ -1,3 +1,5 @@
 example <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8), nrow = 4, ncol = 2)
 
 example <- matrix(c(1, 1, 2, 3, 5, 8, 13, 21), nrow = 4, ncol = 2)
+
+example <- matrix(c(1, 1, 2, 3, 5, 8, 13, 100000), nrow = 4, ncol = 2)
